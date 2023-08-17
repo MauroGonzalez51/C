a2ad9f4835a8599adc20e726b6dcedceaedf3172
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2023-08-17 14:41:11 -0500

    Stack "Finished"

--- Code/Stack/main.c
+++ Code/Stack/main.c
@@ -1,39 +1,87 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 
-#define EXIT_SUCCESS 0
 #define RETURN_SUCCESS 1
 #define RETURN_FAILURE 0
 
-struct Element {
+typedef struct Element {
     int number;
     struct Element *nextElement;
-};
+} Element;
 
-struct Stack {
+typedef struct Stack {
     struct Element *front;
     unsigned int size;
-};
+} Stack;
 
-int createStack(struct Stack *);
-int push(struct Stack *, struct Element element);
+int createStack(Stack *);
+int push(Stack *, Element element);
+Element* pop(Stack *);
+void printStack(Stack *);
+int size(Stack *stack);
 
 int main(void) {
-    struct Stack stack;
+    Stack stack;
 
     createStack(&stack);
 
-    return 0;
+    for (int i = 0; i < 10; i++) {
+        Element element;
+        element.number = i;
+        push(&stack, element);
+    }
+
+    printStack(&stack);
+
+    Element *element;
+    while ((element = pop(&stack)) != NULL) {
+        free(element);
+    }
+
+    return EXIT_SUCCESS;
 }
 
-int createStack(struct Stack *stack) {
+int createStack(Stack *stack) {
     stack -> front = NULL;
     stack -> size = 0;
     return RETURN_SUCCESS;
 }
 
-int push(struct Stack *stack, struct Element element) {
-    struct Element newElement = (struct element *) malloc(sizeof(element)); 
+int push(Stack *stack, Element element) {
+    Element *newElement = (Element *) malloc(sizeof(Element)); 
+
+    if (newElement == NULL) { return RETURN_FAILURE; }
+
+    newElement -> number = element.number;
+    newElement -> nextElement = stack -> front;
+
+    stack -> front = newElement;
+    stack -> size++;
 
     return RETURN_SUCCESS;
+}
+
+Element* pop(Stack *stack) {
+    if (stack -> front == NULL) {
+        return NULL;
+    }
+
+    Element *poppedElement = stack -> front;
+    stack -> front = poppedElement -> nextElement;
+    stack -> size--;
+
+    return poppedElement;
+}
+
+int size(Stack *stack) {
+    return stack -> size;
+}
+
+void printStack(Stack *stack) {
+    Stack tempStack = *stack;
+
+    while (tempStack.front != NULL) {
+        Element *element = pop(&tempStack);
+        printf("Stack [%d]\n", element -> number);
+    }
 }
